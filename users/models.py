b6b2d7e71e05287ab273aab8a4826d0a0d7f2aab
arsaizdihar <arsadihar@gmail.como>
2021-08-08 18:58:00 +0700

    fix user manager bugs

--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from .managers import CustomUserManager
+
 
 class User(AbstractUser):
     username = None
@@ -14,4 +16,6 @@ class User(AbstractUser):
     role = models.CharField(verbose_name="User Role", max_length=10, choices=(
         ("DONATUR", "DONATUR"), ("FUNDRAISER", "FUNDRAISER")), null=True, blank=True)
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ["first_name", "last_name"]
+
+    objects = CustomUserManager()
