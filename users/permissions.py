c4cda840b33d951e7d8bb6190942c880ed645e07
Ahmadhi <ahmadhiprananta@gmail.com>
2021-08-10 10:34:53 +0700

    Update permissions isFundraiser

--- users/permissions.py
+++ users/permissions.py
@@ -2,7 +2,4 @@ from rest_framework import permissions
 
 class isFundraiser(permissions.BasePermission):
     def has_permission(self, request, view):
-        return bool(
-            (request.user.role == "FUNDRAISER") or 
-            (request.method in permissions.SAFE_METHODS)
-        )
+        return bool(request.user.role == "FUNDRAISER")
