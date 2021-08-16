8c8b97b2ec7320bacdff991cef116ae20e40f48b
recedivies <ahmadhiprananta@gmail.com>
2021-08-16 21:13:37 +0700

    Update Fundraiser Register Request By Id

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework_simplejwt.views import (TokenObtainPairView,
                                             TokenRefreshView)
-from users.views import FundraiserRequestView, MeView, RegisterView
+from users.views import FundraiserRequestView, MeView, RegisterView, FundraiserRequestByIdView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
@@ -9,6 +9,7 @@ urlpatterns = [
     path('refresh/', TokenRefreshView.as_view(), name="refresh"),
     path('admin/fundraiser-requests/',
          FundraiserRequestView.as_view(), name="fundraiser-requests"),
+    path('admin/fundraiser-requests/<int:pk>/', FundraiserRequestByIdView.as_view(), name='fundraiser-request-id'),
     path('me/', MeView.as_view(), name="me"),
     path('', include('campaign.urls')),
     path('', include('wallet.urls'))
