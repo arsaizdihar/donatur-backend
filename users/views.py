9b06a5763858377de0a2113303375b5371542d84
Ahmadhi <ahmadhiprananta@gmail.com>
2021-08-16 21:16:42 +0700

    Update Fundraiser Register Request By Id (#9)

--- users/views.py
+++ users/views.py
@@ -5,7 +5,7 @@ from rest_framework_simplejwt.tokens import RefreshToken
 
 from .models import FundraiserProposal, User
 from .serializers import (FundraiserRequestSerializer, MeSerializer,
-                          RegisterSerializer)
+                          RegisterSerializer, FundraiserRequestByIdSerializer)
 
 
 class RegisterView(generics.CreateAPIView):
@@ -33,8 +33,8 @@ class RegisterView(generics.CreateAPIView):
 class FundraiserRequestView(generics.ListAPIView, generics.UpdateAPIView):
     """
     ADMIN ONLY
-    GET         api/fundraiser-requests/ - List of fundraiser registration requests
-    PUT, PATCH  api/fundraiser-requests/ - Verify fundraiser registration by user id
+    GET         api/admin/fundraiser-requests/ - List of fundraiser registration requests
+    PUT, PATCH  api/admin/fundraiser-requests/ - Verify fundraiser registration by user id
     """
 
     permission_classes = (permissions.IsAdminUser, )
@@ -44,6 +44,28 @@ class FundraiserRequestView(generics.ListAPIView, generics.UpdateAPIView):
     def update(self, request, *args, **kwargs):
         user_id = request.data.get("id")
         fundraiser_to_verify = get_object_or_404(self.queryset, id=user_id)
+        fundraiser_to_verify.verify_fundraiser()
+        return Response({"success": True})
+
+
+class FundraiserRequestByIdView(generics.RetrieveUpdateAPIView):
+    """
+    ADMIN ONLY
+    GET         api/admin/fundraiser-requests/<int:id>/ - Retrieve of fundraiser registration request
+    PUT, PATCH  api/admin/fundraiser-requests/<int:id>/ - Verify fundraiser registration by user id
+    """
+
+    permission_classes = (permissions.IsAdminUser, )
+    serializer_class = FundraiserRequestByIdSerializer
+    queryset = User.objects.filter(role="FUNDRAISER", verified=False)
+
+    def get(self, request, pk):
+        fundraiser_to_verify = get_object_or_404(self.queryset, id=pk)
+        serializer = self.get_serializer(fundraiser_to_verify, many=False)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def update(self, request, pk, *args, **kwargs):
+        fundraiser_to_verify = get_object_or_404(self.queryset, id=pk)
         fundraiser_to_verify.verify_fundraiser()
         return Response({"success": True})
 
